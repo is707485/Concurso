d8c2c0a27fcfaf996a6fe5d49e4833734c557322
is707485 <49995946+is707485@users.noreply.github.com>
2019-09-21 19:14:52 -0500

    Update CGV_Tren.java.java

--- CGV_Tren.java.java
+++ CGV_Tren.java.java
@@ -1,57 +1,33 @@
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.*;
 
-public class Problemas {
-	static int mov = 0;
-	static int cmp = 0;
+/**
+ *
+ * @author Luis
+ */
+public class JavaApplication77 {
 
-	static boolean greaterThan(int a, int b) {
-		cmp ++;
-		return a > b;
-	}
-
-	public static void insertion(int[] array, int delta, int start) {
-		for (int p = start + delta; p < array.length; p += delta) {
-			int pivot = array[p];
-			int i = p - delta;
-			while (i >= start && greaterThan(array[i], pivot)) { // array[i]>pivot) {
-				array[i + delta] = array[i];
-				mov++;
-				i = i - delta;
-			}
-			array[i + delta] = pivot;
-		}
-		System.out.println("\nMov: "+mov);
-	}
-
-	public static void shell(int[] array) {
-		mov = 0;
-		cmp = 0;
-		int delta = 1;
-		while(delta < array.length) delta = 3 * delta + 1;
-		delta /= 3;
-		//		delta = 1;  Degradar Shell a Inserción
-		while(delta >= 1) {
-			for(int start = 0; start < delta; start ++) {
-				insertion(array, delta, start);		
-			}
-			delta /= 3;
-		}
-	}
-	public static void main(String[] args) {
-
-		int array1 []= {1,3,2};
-		int array2[]= {4,3,2,1};
-		
-		
-		shell(array1);
-		System.out.print("Array1: ");
-		for(int i =0;i<array1.length;i++) 
-			System.out.print(array1[i]);
-		
-		shell(array2);
-		System.out.print("Array2: ");
-		for(int i =0;i<array2.length;i++)
-			System.out.print(array2[i]);
-	
-	}
-
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[]x = {1,3,2};
+        System.out.println(MoveCounter(x,x.length));
+    }
+    public static int MoveCounter(int[] x,int n){
+        int moves=0;
+        int[] notHash=new int[n];
+        for(int c=0;c<x.length;c++){
+            notHash[x[c]-1]=c+1;
+        }
+        int r=0;
+        for(int c=0;c<notHash.length;c++){
+            if(((notHash[c]+r)-(c+1))>0){
+                moves+=(notHash[c]+r)-(c+1);
+                r++;
+            }
+        }
+        return moves;
+    }
 }
+    
